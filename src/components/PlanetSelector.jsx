42d028f2a0ec07f934b37d53c50240aa9dddafb3
ZH Rifat <contact.zhrifat@gmail.com>
2025-02-16 10:47:44 +0600

    ui

--- src/components/PlanetSelector.jsx
+++ src/components/PlanetSelector.jsx
@@ -3,10 +3,12 @@ import planets from '../data/planets.json';
 
 const PlanetSelector = ({ onSelectPlanet }) => {
   return (
-    <select onChange={(e) => onSelectPlanet(JSON.parse(e.target.value))}>
+    <select onChange={(e) => onSelectPlanet(JSON.parse(e.target.value))}
+      className='planet-selector'
+    >
       <option value="">Select a planet</option>
       {planets.map((planet, index) => (
-        <option key={index} value={JSON.stringify(planet)}>
+        <option key={index} value={JSON.stringify(planet)}        >
           {planet.name}
         </option>
       ))}
